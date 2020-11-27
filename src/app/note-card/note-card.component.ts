56399ee2501f5a78b01586f94874541e88746e70
Kartik Nayak <kartik.nayak@rocketmail.com>
2020-11-28 00:06:20 +0400

    inti

---
+++ src/app/note-card/note-card.component.ts
@@ -0,0 +1,14 @@
+import { viewClassName } from '@angular/compiler';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-note-card',
+  templateUrl: './note-card.component.html',
+  styleUrls: ['./note-card.component.scss']
+})
+export class NoteCardComponent implements OnInit {
+  @Input() title: string;
+  @Input() body: string;
+  ngOnInit() {
+}
+}
